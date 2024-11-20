f0f4629d319ca0411b0f3c8ed6efb25d3e1c23e8
Xghng <135593952+Xghng@users.noreply.github.com>
2024-11-20 16:03:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-echo 'src-git helloworld https://github.com/LemonCrab666/openwrt-luci-app-openvpn-server' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# echo 'src-git helloworld https://github.com/LemonCrab666/openwrt-luci-app-openvpn-server' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
